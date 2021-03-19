5e21e0b70ff21512ab04d653c69afbfa31cd0438
ABH <aimless_pointer@yahoo.com>
2021-03-18 19:30:59 -0700

    Changed file

--- hello.go
+++ hello.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+func testFunc(messageChanger string) {
+	fmt.Printf("You passed this string in: %s", messageChanger)
+}
+
 func main() {
-
-	fmt.Println("Hello, world.")
-
+	testFunc("Blah")
 }
